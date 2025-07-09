86a271d59d8768fad1874713f3805546daf1d696
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-07-09 18:52:24 +0100

    Add files via upload

---
+++ JOptionPane/src/DSA/messageDialogue.java
@@ -0,0 +1,19 @@
+package DSA;
+
+import javax.swing.*;
+
+public  class messageDialogue {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome to Java Programming");
+        JButton.getDefaultLocale();
+        JOptionPane.showMessageDialog(null, "Welcome to Java Programming", "Welcome", JOptionPane.INFORMATION_MESSAGE);
+        String name = JOptionPane.showInputDialog("Enter your name: ");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+        String Id = JOptionPane.showInputDialog("Enter your id: ");
+
+
+
+
+
+    }
+}
