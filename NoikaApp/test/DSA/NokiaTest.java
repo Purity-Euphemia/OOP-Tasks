eb444de4d6db8e2178d012d845cade7273fb4c73
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-07-20 04:24:41 +0100

    Add files via upload

--- NoikaApp/test/DSA/NokiaTest.java
+++ NoikaApp/test/DSA/NokiaTest.java
@@ -24,4 +24,45 @@ public class NokiaTest {
         nokia.turnOff();
         assertFalse(nokia.getStatus());
     }
+    @Test
+    public void testThatRemindersCanAddRemainder(){
+        RemaindersMenu remaindersMenu = new RemaindersMenu();
+        String note = "Test Reminder";
+        String result = remaindersMenu.addRemainder(note);
+        assertEquals("Remainder added: Test Reminder", result);
+    }
+    @Test
+    public void testThatRemindersCanDeleteRemainder(){
+        RemaindersMenu remaindersMenu = new RemaindersMenu();
+        String note = "Test Reminder";
+        remaindersMenu.addRemainder(note);
+        String result = remaindersMenu.deleteRemainder(note);
+        assertEquals("Remainder deleted: Test Reminder", result);
+    }
+    @Test
+    public void testThatRemindersCanListRemainders() {
+        RemaindersMenu remaindersMenu = new RemaindersMenu();
+        String note = "Test Reminder";
+        remaindersMenu.addRemainder(note);
+        String result = remaindersMenu.viewRemainders();
+        assertEquals("Test Reminder", result);
+    }
+    @Test
+    public void testProfileCanBeSelected(){
+        Profiles profiles = new Profiles();
+        String result = profiles.selectProfiles("Silent");
+        assertEquals("Profile selected: Silent", result);
+    }
+    @Test
+    public void testProfilesCanChangeProfile(){
+        Profiles profiles = new Profiles();
+        String result = profiles.selectProfiles("Meeting");
+        assertEquals("Profile selected: Meeting", result);
+    }
+    @Test
+    public void testProfilesGetDefualtProfile(){
+        Profiles profiles = new Profiles();
+        String result = profiles.selectProfiles("General");
+        assertEquals("Profile selected: General", result);
+    }
 }
