f4fdb631a82412355e280d73a8a0d8ca1f3f6a72
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-05 07:01:01 +0100

    Add files via upload

--- DrugsManagementApp/src/DSA/PrescribedDrug.java
+++ DrugsManagementApp/src/DSA/PrescribedDrug.java
@@ -1,4 +1,23 @@
 package DSA;
 
 public class PrescribedDrug {
+    private Drug drug;
+    private int quantity;
+
+    public PrescribedDrug(Drug drug, int quantity) {
+        this.drug = drug;
+        this.quantity = quantity;
+    }
+    public Drug getDrug() {
+        return drug;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setDrug(Drug drug) {
+        this.drug = drug;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
