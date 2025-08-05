e2743ab4bacda991b2cda8f2de283e31c96b3044
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-05 05:15:53 +0100

    Add files via upload

--- DrugsManagementApp/src/DSA/Drug.java
+++ DrugsManagementApp/src/DSA/Drug.java
@@ -7,11 +7,42 @@ public class Drug {
     private Category category;
     private Type type;
 
-    public Drug(int id, String name, Date expiry, Category category, Type type){
+    public Drug(int id, String name, Date expiry, Category category, Type type) {
         this.id = id;
         this.name = name;
         this.expiry = expiry;
         this.category = category;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public Date getExpiry() {
+        return expiry;
+    }
+    public Category getCategory() {
+        return category;
+    }
+    public Type getType() {
+        return type;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setExpiry(Date expiry) {
+        this.expiry = expiry;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    public void setType(Type type) {
         this.type = type;
     }
 }
--- DrugsManagementApp/src/DSA/DrugInventory.java
+++ DrugsManagementApp/src/DSA/DrugInventory.java
@@ -1,4 +1,24 @@
 package DSA;
 
 public class DrugInventory {
+    private Drug drug;
+    private int quantity;
+
+    public DrugInventory(Drug drug, int quantity) {
+        this.drug = drug;
+        this.quantity = quantity;
+    }
+    public Drug getDrug() {
+        return drug;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setDrug (Drug drug) {
+        this.drug = drug;
+    }
+    public void setQuantity (int quantity) {
+        this.quantity = quantity;
+    }
+
 }
