dd546df4035c97ec514f89e8b9b977f17b9e18a9
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-06 02:25:27 +0100

    Add files via upload

--- DrugsManagementApp/src/DSA/Prescription.java
+++ DrugsManagementApp/src/DSA/Prescription.java
@@ -1,4 +1,33 @@
 package DSA;
+import java.util.List;
 
 public class Prescription {
+    private int id;
+    private String doctor;
+    private List<PrescribedDrug> prescribedDrugs;
+
+    public Prescription(int id, String doctor, List<PrescribedDrug> prescribedDrugs) {
+        this.id = id;
+        this.doctor = doctor;
+        this.prescribedDrugs = prescribedDrugs;
+    }
+
+    public int getid() {
+        return id;
+    }
+    public String getDoctor() {
+        return doctor;
+    }
+    public List<PrescribedDrug> prescribedDrugs() {
+        return prescribedDrugs;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+    public void setPrescribedDrugs(List<PrescribedDrug> prescribedDrugs) {
+        this.prescribedDrugs = prescribedDrugs;
+    }
 }
--- DrugsManagementApp/src/DSA/Receipt.java
+++ DrugsManagementApp/src/DSA/Receipt.java
@@ -1,4 +1,33 @@
 package DSA;
 
+import java.util.Date;
+
 public class Receipt {
+    private int id;
+    private Date date;
+    private Prescription prescription;
+
+    public Receipt(int id, Date date, Prescription prescription) {
+        this.id = id;
+        this.date = date;
+        this.prescription = prescription;
+    }
+    public int getId() {
+        return id;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public Prescription getPrescription() {
+        return prescription;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public void setPrescription(Prescription prescription) {
+        this.prescription = prescription;
+    }
 }
