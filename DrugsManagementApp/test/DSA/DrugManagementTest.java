9748e73d0b8940e53233cac207dde87243fd31c4
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-06 04:43:04 +0100

    Add files via upload

--- DrugsManagementApp/test/DSA/DrugManagementTest.java
+++ DrugsManagementApp/test/DSA/DrugManagementTest.java
@@ -1,4 +1,20 @@
 package DSA;
 
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DrugManagementTest {
+
+    @Test
+    public void testAddDrug_IncreasesInventory() {
+        DrugManagement manager = new DrugManagement();
+        Drug drug = new Drug(1, "Paracetamol", new Date(2023, 5, 1), new Category("Antibiotic"), new Type("Capsule"));
+        manager.addDrug(drug, 75);
+        assertEquals(1, manager.getInventory().size());
+    }
+
 }
