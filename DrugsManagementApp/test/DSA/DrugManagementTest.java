ea157d113747c2788747fc6fa26cd82a21c03ded
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-07 02:11:25 +0100

    Add files via upload

--- DrugsManagementApp/test/DSA/DrugManagementTest.java
+++ DrugsManagementApp/test/DSA/DrugManagementTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DrugManagementTest {
 
@@ -15,6 +15,25 @@ public class DrugManagementTest {
         Drug drug = new Drug(1, "Paracetamol", new Date(2023, 5, 1), new Category("Antibiotic"), new Type("Capsule"));
         manager.addDrug(drug, 75);
         assertEquals(1, manager.getInventory().size());
+    }
+    @Test
+    public void testAddTwoDrugs_IncreasesInventory() {
+        DrugManagement manager = new DrugManagement();
+        Drug drug = new Drug(1, "Paracetamol", new Date(2023, 5, 1), new Category("Antibiotic"), new Type("Capsule"));
+        manager.addDrug(drug, 75);
+        assertEquals(1, manager.getInventory().size());
+        Drug drug2 = new Drug(2, "Iron supplement", new Date(2024, 3, 15), new Category("Anemia"), new Type("Syrup"));
+        manager.addDrug(drug2, 100);
+        assertEquals(2, manager.getInventory().size());
+    }
+    @Test
+    public void testFindDrugByNameExists() {
+        DrugManagement manager = new DrugManagement();
+        Drug drug = new Drug(3, "Amoxicillin", new Date(2030, 4, 20), new Category("Antibiotic"), new Type("Capsule"));
+        manager.addDrug(drug, 100);
+        DrugInventory found = manager.findDrugByName("Amoxicillin");
+        assertNotNull(found);
+        assertEquals("Amoxicillin", found.getDrug().getName());
     }
 
 }
