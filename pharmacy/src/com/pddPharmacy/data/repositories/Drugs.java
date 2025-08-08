21590829a9b6e12718bf036425e3ddf9eeeb2be2
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-08 02:28:15 +0100

    Add files via upload

--- pharmacy/src/com/pddPharmacy/data/repositories/Drugs.java
+++ pharmacy/src/com/pddPharmacy/data/repositories/Drugs.java
@@ -1,4 +1,33 @@
 package com.pddPharmacy.data.repositories;
 
+import com.pddPharmacy.data.models.Drug;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Drugs {
+    private List<Drug> drugs = new ArrayList<>();
+
+    public long count() {
+        return drugs.size();
+    }
+
+    public Drug save(Drug drug) {
+        drug.setId(generateId());
+        drugs.add(drug);
+        return drug;
+    }
+
+    private int generateId() {
+        return drugs.size() + 1;
+    }
+
+    public Drug findById(int id) {
+        for (Drug drug : drugs) {
+            if (drug.getId() == id) {
+                return drug;
+            }
+        }
+        return null; 
+    }
+
 }
