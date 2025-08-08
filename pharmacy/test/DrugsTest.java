662816397fcc2728fb03bd58f44f170ac5e363e0
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-08 02:29:07 +0100

    Add files via upload

--- pharmacy/test/DrugsTest.java
+++ pharmacy/test/DrugsTest.java
@@ -1,2 +1,32 @@
+import com.pddPharmacy.data.models.Drug;
+import com.pddPharmacy.data.repositories.Drugs;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DrugsTest {
+    private Drugs drugs;
+
+    @BeforeEach
+    void setup() {
+        drugs = new Drugs();
+    }
+    @Test
+    public void addDrugs_countIsOne() {
+        assertEquals(0,drugs.count());
+        Drug panadol = new Drug();
+        drugs.save(panadol);
+        assertEquals(1,drugs.count());
+    }
+    @Test
+    public void addDrugs_findDrugsByIdReturnsDrugTest() {
+        Drug panadol = new Drug();
+        Drug savedDrug = drugs.save(panadol);
+        Drug foundDrug = drugs.findById(savedDrug.getId());
+        assertEquals(savedDrug, foundDrug);
+    }
+
+
+
 }
