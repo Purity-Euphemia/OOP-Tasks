e63203412a006a52c4d87de24ae92a55df78ca0f
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-10 03:39:09 +0100

    Add files via upload

--- pharmacy/src/com/pddPharmacy/data/repositories/Drugs.java
+++ pharmacy/src/com/pddPharmacy/data/repositories/Drugs.java
@@ -11,7 +11,7 @@ public class Drugs {
         return drugs.size();
     }
 
-    public Drug save(Drug drug) {
+    public Drug savedNew(Drug drug) {
         drug.setId(generateId());
         drugs.add(drug);
         return drug;
@@ -21,13 +21,42 @@ public class Drugs {
         return drugs.size() + 1;
     }
 
+    public Drug save(Drug drug) {
+        if (isNew(drug)) {
+            savedNew(drug);
+        } else {
+            update(drug);
+        }
+        return drug;
+    }
+
+    private boolean isNew(Drug drug) {
+        if (drug.getId() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private  void update(Drug drug) {
+        deleteById(drug.getId());
+        drugs.add(drug);
+    }
+
+    private  void deleteById(int id) {
+        for (int count = 0; count < drugs.size(); count++) {
+            if (drugs.get(count).getId() == id) {
+                drugs.remove(count);
+                return;
+            }
+        }
+    }
     public Drug findById(int id) {
-        for (Drug drug : drugs) {
+        for (var drug : drugs) {
             if (drug.getId() == id) {
                 return drug;
             }
         }
-        return null; 
+        return null;
     }
 
 }
