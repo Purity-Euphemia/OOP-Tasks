acff095ea8d180e5b7acf255c60d8d337f938414
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-15 05:33:45 +0100

    Add files via upload

--- pharmacy/src/com/pddPharmacy/services/PharmacistServices.java
+++ pharmacy/src/com/pddPharmacy/services/PharmacistServices.java
@@ -1,12 +1,21 @@
 package com.pddPharmacy.services;
 
+
+
+import com.pddPharmacy.data.models.Drug;
+import com.pddPharmacy.data.repositories.Drugs;
+import com.pddPharmacy.dtos.request.AddDrugRequest;
 import com.pddPharmacy.dtos.response.AddDrugResponse;
 
 import java.time.LocalDate;
 
 public class PharmacistServices {
-    public void addDrug(String name, String type, String category, LocalDate expiry) {
-    }
+   private Drugs drugs = new Drugs();
 
-    public AddDrugResponse addDrug(addDrugRequest)
+
+   public AddDrugResponse addDrug(AddDrugRequest addDrugRequest) {
+       Drug drug = map(addDrugRequest);
+       Drug savedDrug = drugs.save(drug);
+       return null;
+   }
 }
