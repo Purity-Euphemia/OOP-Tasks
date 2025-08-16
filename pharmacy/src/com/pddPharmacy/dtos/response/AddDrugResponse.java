4d4df001b7d6791c4e481d8071d1f862d5fa43e3
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-16 02:41:18 +0100

    Add files via upload

--- pharmacy/src/com/pddPharmacy/dtos/response/AddDrugResponse.java
+++ pharmacy/src/com/pddPharmacy/dtos/response/AddDrugResponse.java
@@ -1,4 +1,19 @@
 package com.pddPharmacy.dtos.response;
 
 public class AddDrugResponse {
+    private int drugId;
+    private String drugName;
+
+    public int getDrugId() {
+        return drugId;
+    }
+    public void setDrugId(int drugId) {
+        this.drugId = drugId;
+    }
+    public String getDrugName() {
+        return  drugName;
+    }
+    public void setDrugName(String drugName) {
+        this.drugName = drugName;
+    }
 }
