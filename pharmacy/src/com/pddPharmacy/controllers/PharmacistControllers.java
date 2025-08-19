96cd7188a84bc69dbd08d34bc0ab449e71c7959a
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-19 22:07:12 +0100

    Add files via upload

--- pharmacy/src/com/pddPharmacy/controllers/PharmacistControllers.java
+++ pharmacy/src/com/pddPharmacy/controllers/PharmacistControllers.java
@@ -7,13 +7,19 @@ import com.pddPharmacy.dtos.response.AddDrugResponse;
 import com.pddPharmacy.services.PharmacistServices;
 
 public class PharmacistControllers {
-    private PharmacistServices pharmacistServices;
-    private Drugs drugs;
+    private PharmacistServices pharmacistServices = new PharmacistServices();
+
 
     public AddDrugResponse recordNewDrug(AddDrugRequest addDrugRequest) {
         return  pharmacistServices.addDrug(addDrugRequest);
     }
-
+    public String buyDrug(BuyDrugsRequest buyDrugsRequest) {
+        pharmacistServices.buyDrugs(buyDrugsRequest);
+        return "Purchased";
+    }
+    public List<AvailableDrugResponse> getAvailableDrugs(){
+        return pharmacistServices.getAvailableDrugs();
+    }
 
 }
 
